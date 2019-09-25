bfaea67aa448c18d6145b96944919846e06cc98e
Lautaro Carro <luisb@lagash.com>
2019-09-25 19:25:00 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6f144b1964022baad6be3577c9dcb29.js"
+  "/precache-manifest.d392d7838f05c81891aef6416f8fbcb1.js"
 );
 
 self.addEventListener('message', (event) => {
